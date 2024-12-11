882fbe0572c2aca9755b366d5051577460953c95
ardiansyah999 <131391384+ardiansyah999@users.noreply.github.com>
2024-12-12 01:07:37 +0700

    tes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+
 }
 
 android {
@@ -35,18 +36,13 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        mlModelBinding = true
     }
 }
 
 dependencies {
 
     implementation (libs.material.v190)
-    implementation(libs.glide)
-    implementation(libs.retrofit)
-    implementation(libs.converter.gson)
-    implementation(libs.logging.interceptor)
-
-
 
     implementation(libs.androidx.cardview)
     implementation(libs.androidx.core.ktx)
@@ -57,9 +53,12 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
-    implementation(libs.androidx.legacy.support.v4)
-    implementation(libs.androidx.fragment.ktx)
+    implementation(libs.tensorflow.lite.support)
+    implementation(libs.tensorflow.lite.metadata)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+
+  
 }
--- app/src/main/java/com/example/wijiwaste/data/NewsItem.kt
+++
@@ -1,8 +0,0 @@
-package com.example.wijiwaste.data
-
-data class NewsItem(
-    val title: String,
-    val description: String,
-    val image: String,
-    val pubDate: String
-)
